9317b586360da9fae32b423d1a68a27d11599ecc
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-03 13:47:22 +0300

    Initial Commit

---
+++ AnimatedText/AnimatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTextApp.swift
+//  AnimatedText
+//
+//  Created by Danil Peregorodiev on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
