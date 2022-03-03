93c78b65d5ad3159f91a84d597be21a4a2a197d8
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-03 23:12:54 +0300

    Sus

--- AnimatedText/ContentView.swift
+++ AnimatedText/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    @State var multiColor = false
+    
+    var body: some View {
+        
+        VStack(spacing: 25) {
+            TextShimer(multiColors: $multiColor, text: "South.Russian")
+            
+            Toggle(isOn: $multiColor, label: {
+                Text("Разноцветный режим")
+                .font(.title3)
+                .fontWeight(.bold)
+            })
+            .padding()
+                
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct TextShimer: View {
+    @State var animation: Bool = false
+    @Binding var multiColors: Bool
+    var text: String
+    var body: some View {
+        
+        ZStack {
+            Text(text)
+                .font(.system(size: 50, weight: .bold))
+                .foregroundColor(Color.white.opacity(0.25))
+            
+            HStack(spacing: 0) {
+                ForEach(0..<text.count, id: \.self) { index in
+                    
+                    Text(String(text[text.index(text.startIndex, offsetBy: index)]))
+                        .foregroundColor(multiColors ? randomColor() : Color.white)
+                }
+            }
+            .mask(
+                Rectangle()
+                    .fill(
+                        LinearGradient(gradient: .init(colors:[Color.white.opacity(0.5), Color.white, Color.white.opacity(0.5)]), startPoint: .top, endPoint: .bottom))
+                    .rotationEffect(.init(degrees: 70))
+                    .padding(20)
+                    .offset(x: 250)
+                    .offset(x: animation ? 500 : 0)
+            )
+            .onAppear(perform: {
+                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                    
+                    animation.toggle()
+                }
+            })
+        }
+    }
+    func randomColor()->Color {
+        let color = UIColor(red: 1, green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        return Color(color)
     }
 }
